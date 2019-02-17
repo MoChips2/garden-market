1244b7fc8e73faefdf3d150c0b75aa0340585590
Brian Nickila <brian.nickila@gmail.com>
2019-02-16 18:34:13 -0600

    new init

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(logger("dev"));
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/googlebooks", { useNewUrlParser: true });
+
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
+
+
 
 // Send every other request to the React app
 // Define any API routes before this runs
