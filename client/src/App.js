a0323aaa42afa9bfb47a2ea93b6546943580ef36
Brian Nickila <brian.nickila@gmail.com>
2019-02-22 23:53:56 -0600

    Setting up folder structure.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/searchresult" component={SearchResult} />
+            <Route exact path="/newmarket" component={NewMarket} />
+            <Route exact path="/market/:id" component={Market} />
+            <Route component={NoMatch} />
+          </Switch>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from ("react");
+
+function Home() {
+
+    return (
+        <div>
+            <h1>This is our Home Page</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tincidunt sit amet diam et ullamcorper. Curabitur vitae dolor feugiat, commodo lacus vitae, cursus ligula. Curabitur ut tristique tellus. Mauris lacus arcu, posuere vitae arcu at, mollis laoreet nunc. Donec a ipsum sit amet augue aliquet mattis ut sed nulla. Nam blandit quis velit nec suscipit. Aenean sit amet accumsan justo, vel rutrum tortor. Donec blandit venenatis purus quis suscipit. Proin vehicula mauris at lectus accumsan tempus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dui eros, laoreet sit amet dolor ac, pretium blandit libero. Maecenas imperdiet est neque, eu vulputate lacus suscipit sit amet. Maecenas massa leo, auctor nec tellus accumsan, hendrerit sagittis eros. Proin aliquam justo sed fringilla fermentum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in dictum felis. Nunc sed justo id purus placerat fermentum. Fusce ultrices consectetur arcu, et semper sem molestie quis. Morbi in elit eget nunc sollicitudin pharetra. Suspendisse eu erat aliquet, vestibulum ante vel, volutpat metus. Cras ac est felis. Quisque quis imperdiet tortor, porttitor semper tortor. In libero nunc, vehicula quis imperdiet ut, tempus a metus. Fusce nunc felis, egestas at volutpat ac, semper non tellus. Etiam eu sapien ex.</p>
+        </div>
+
+    )
+}
+
+export default Home;
