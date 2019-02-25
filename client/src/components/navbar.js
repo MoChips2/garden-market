96fc612eecdb691591693581ed64dd6d79034a12
Brian Nickila <brian.nickila@gmail.com>
2019-02-25 07:43:34 -0600

    Working on grabbing all data from new market form. Almost there.

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -4,7 +4,7 @@ import React from "react";
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">Garden App</a>
+            <a className="navbar-brand" href="/home">Garden App</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
@@ -12,24 +12,16 @@ function Navbar() {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        <a className="nav-link" href="/home">Home <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a className="dropdown-item" href="#">Action</a>
-                            <a className="dropdown-item" href="#">Another action</a>
-                            <div className="dropdown-divider"></div>
-                            <a className="dropdown-item" href="#">Something else here</a>
-                        </div>
+                        <a className="nav-link" href="/newmarket">NewMarket</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        <a className="nav-link" href="/login">Login</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/market">Market</a>
                     </li>
                 </ul>
                 <form className="form-inline my-2 my-lg-0">
--- client/src/pages/NewMarket.js
+++ client/src/pages/NewMarket.js
@@ -1,106 +1,160 @@
-import React from "react";
+import React, { Component } from "react";
 
-function NewMarket() {
+class NewMarket extends Component {
+    state = {
+        marketName: "",
+        name: "",
+        email: "",
+        address: "",
+        city: "",
+        state: "",
+        zip: "",
+        about: "",
+        roles: []
 
-    return (
-        <div>
+    };
 
-            <div className="container new-market-container">
-                <div className="row">
-                    <div className="col-md-6">
-                        <h1>New Market Page</h1>
-                        <p>This will contain a form to create a new market.</p>
-                        <form>
-                            <div className="form-row">
-                                <div className="form-group col-md-12">
-                                    <label>Market Name</label>
-                                    <input type="text" className="form-control" id="inputname4" placeholder="Example Market" />
-                                </div>
-                            </div>
-                            <div className="form-row">
-                                <div className="form-group col-md-12">
-                                    <label>Organizer Name</label>
-                                    <input type="text" className="form-control" id="inputPassword4" placeholder="Johnny Appleseed" />
-                                </div>
-                            </div>
-                            <div className="form-row">
-                                <div class="form-group col-md-12">
-                                    <label for="exampleInputEmail1">Email address</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                                </div>
-                            </div>
-                            <div className="form-row">
-                                <div className="form-group col-md-12">
-                                    <label>Address</label>
-                                    <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
-                                </div>
-                            </div>
-                            <div className="form-row">
-                                <div className="form-group col-md-8">
-                                    <label>City</label>
-                                    <input type="text" className="form-control" id="inputCity" />
-                                </div>
-                                <div className="form-group col-md-2">
-                                    <label>State</label>
-                                    <select id="inputState" className="form-control">
-                                        <option selected>Choose...</option>
-                                        <option>...</option>
-                                    </select>
-                                </div>
-                                <div className="form-group col-md-2">
-                                    <label>Zip</label>
-                                    <input type="text" className="form-control" id="inputZip" />
-                                </div>
-                            </div>
-                            <div className="form-row">
-                                <div class="form-group col-md-12">
-                                    <label for="exampleFormControlTextarea1">Tell us about your market:</label>
-                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                                </div>
-                            </div>
-                            <div className="form-group">
-                                <label>Choose all that apply:</label>
-                                <div className="form-check">
-                                    <input className="form-check-input" type="checkbox" id="growerVeg" value="growerVeg" />
-                                    <label className="form-check-label">Grower of Vegetables</label>
-                                </div>
-                                <div className="form-check">
-                                    <input className="form-check-input" type="checkbox" id="growerFruit" value="option2" />
-                                    <label className="form-check-label">Grower of Fruits</label>
-                                </div>
-                                <div className="form-check">
-                                    <input className="form-check-input" type="checkbox" id="landowner" value="landowner" />
-                                    <label className="form-check-label">Landowner</label>
-                                </div>
-                                <div className="form-check">
-                                    <input className="form-check-input" type="checkbox" id="baker" value="baker" />
-                                    <label className="form-check-label">Baker</label>
-                                </div>
-                                <div className="form-check">
-                                    <input className="form-check-input" type="checkbox" id="beekeeper" value="beekeeper" />
-                                    <label className="form-check-label">Beekeeper</label>
-                                </div>
-                                <div className="form-check">
-                                    <input className="form-check-input" type="checkbox" id="poultry" value="poultry" />
-                                    <label className="form-check-label">Poultry Farmer</label>
-                                </div>
-                                <div className="form-check">
-                                    <input className="form-check-input" type="checkbox" id="other" value="other" />
-                                    <label className="form-check-label">Other</label>
-                                </div>
-                            </div>
-                            <button type="submit" className="btn btn-primary">Submit</button>
-                        </form>
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
 
+    onChange(e) {
+        const roles = this.state.roles
+        let index
+        if (e.target.checked) {
+            roles.push(+e.target.value)
+        } else {
+            index = roles.indexOf(+e.target.value)
+            roles.splice(index, 1)
+        }
+        this.setState({ roles: roles })
+    }
+
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        if (this.state.marketName && this.state.name && this.state.email) {
+            console.log("worked!")
+            console.log({
+                marketName: this.state.marketName,
+                name: this.state.name,
+                email: this.state.email,
+                roles: this.state.roles,
+                state: this.state.state,
+                city: this.state.city,
+                zip: this.state.zip,
+                about: this.state.about
+                // roles: this.state.roles
+
+            })
+        }
+    }
+    render() {
+        return (
+            <div>
+                <div className="container new-market-container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <h1>New Market Page</h1>
+                            <p>This will contain a form to create a new market.</p>
+                            <form>
+                                <div className="form-row">
+                                    <div className="form-group col-md-12">
+                                        <label>Market Name</label>
+                                        <input type="text" className="form-control" name="marketName" placeholder="Example Market" value={this.state.marketName} onChange={this.handleInputChange} />
+                                    </div>
+                                </div>
+                                <div className="form-row">
+                                    <div className="form-group col-md-12">
+                                        <label>Organizer Name</label>
+                                        <input type="text" className="form-control" name="name" placeholder="Johnny Appleseed" value={this.state.name} onChange={this.handleInputChange} />
+                                    </div>
+                                </div>
+                                <div className="form-row">
+                                    <div className="form-group col-md-12">
+                                        <label>Email address</label>
+                                        <input type="email" className="form-control" name="email" aria-describedby="emailHelp" placeholder="Enter email" value={this.state.email} onChange={this.handleInputChange} />
+                                        <small name="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                                    </div>
+                                </div>
+                                <div className="form-row">
+                                    <div className="form-group col-md-12">
+                                        <label>Address</label>
+                                        <input type="text" className="form-control" name="address" placeholder="1234 Main St" value={this.state.address} onChange={this.handleInputChange} />
+                                    </div>
+                                </div>
+                                <div className="form-row">
+                                    <div className="form-group col-md-8">
+                                        <label>City</label>
+                                        <input type="text" className="form-control" name="city" value={this.state.city} onChange={this.handleInputChange} />
+                                    </div>
+                                    <div className="form-group col-md-2">
+                                        <label>State</label>
+                                        <select name="state" className="form-control">
+                                            <option>Choose...</option>
+                                            <option>MN</option>
+                                            <option>WI</option>
+                                        </select>
+                                    </div>
+                                    <div className="form-group col-md-2">
+                                        <label>Zip</label>
+                                        <input type="text" className="form-control" name="zip" value={this.state.zip} onChange={this.handleInputChange} />
+                                    </div>
+                                </div>
+                                <div className="form-row">
+                                    <div className="form-group col-md-12">
+                                        <label>Tell us about your market:</label>
+                                        <textarea className="form-control" name="about" rows="3" value={this.state.about} onChange={this.handleInputChange}></textarea>
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <label>Choose all that apply:</label>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="checkbox" name="roles" value="growerVeg" onChange={this.onChange.bind(this)} />
+                                        <label className="form-check-label">Grower of Vegetables</label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="checkbox" name="roles" value="growerFruit" onChange={this.onChange.bind(this)} />
+                                        <label className="form-check-label">Grower of Fruits</label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="checkbox" name="roles" value="landowner" onChange={this.onChange.bind(this)} />
+                                        <label className="form-check-label">Landowner</label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="checkbox" name="roles" value="baker" onChange={this.onChange.bind(this)} />
+                                        <label className="form-check-label">Baker</label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="checkbox" name="roles" value="beekeeper" onChange={this.onChange.bind(this)} />
+                                        <label className="form-check-label">Beekeeper</label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="checkbox" name="roles" value="poultry" onChange={this.onChange.bind(this)} />
+                                        <label className="form-check-label">Poultry Farmer</label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="checkbox" name="roles" value="other" onChange={this.onChange.bind(this)} />
+                                        <label className="form-check-label">Other</label>
+                                    </div>
+                                </div>
+                                <button type="submit" className="btn btn-primary" onClick={this.handleFormSubmit}>Submit</button>
+                            </form>
+
+                        </div>
                     </div>
                 </div>
+
+
             </div>
 
 
-        </div>
 
-    )
+        )
+    }
 }
 
 export default NewMarket;
