66c3a2054d8fd92d0ef35108dfe3524faa88bb1b
Brian Nickila <brian.nickila@gmail.com>
2019-02-25 20:31:25 -0600

    Added links

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import Link from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
@@ -11,27 +11,27 @@ function Navbar() {
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/home">Home <span className="sr-only">(current)</span></a>
+                    <li className={window.location.pathname === "/home" ? "active" : ""}>
+                        <Link className="nav-link" to="/home">Home </Link>
                     </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/newmarket">NewMarket</a>
+                    <li className={window.location.pathname === "/newmarket" ? "active" : ""}>
+                        <Link className="nav-link" to="/newmarket">NewMarket</Link>
                     </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/login">Login</a>
+                    <li className={window.location.pathname === "/login" ? "active" : ""}>
+                        <Link className="nav-link" to="/login">Login</Link>
                     </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/market">Market</a>
+                    <li className={window.location.pathname === "/market" ? "active" : ""}>
+                        <Link className="nav-link" to="/market">Market</Link>
                     </li>
                 </ul>
                 <form className="form-inline my-2 my-lg-0">
                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
 
-            )
+    )
 }
 
 export default Navbar;
