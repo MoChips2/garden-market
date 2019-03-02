abdbc37ec78982ab3ca965e50bdd4e43c4313db6
shilpipawar <shilpipawar@gmail.com>
2019-03-01 23:17:58 -0600

    deleted key.js

--- client/src/key.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-console.log('keys.js is loaded');
-
-exports.googleAPI = {
-  apiKey:process.env.API_KEY
-}
-
-//module.exports = spotify;
-console.log('End Loading keys.js..');
