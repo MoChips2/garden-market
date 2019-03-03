21f666547541f9c811789f3eb3902a32e97a8b4b
Brian Nickila <brian.nickila@gmail.com>
2019-03-02 18:58:48 -0600

    Little changes.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,16 +6,16 @@ function Navbar() {
     return (
         
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="/home">Garden App</a>
+            <Link className="navbar-brand" to="/">Garden App</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
-                    <li className={window.location.pathname === "/" ? "active" : ""}>
+                    {/* <li className={window.location.pathname === "/" ? "active" : ""}>
                         <Link className="nav-link" to="/">Home </Link>
-                    </li>
+                    </li> */}
                     <li className={window.location.pathname === "/newmarket" ? "active" : ""}>
                         <Link className="nav-link" to="/newmarket">NewMarket</Link>
                     </li>
@@ -26,11 +26,11 @@ function Navbar() {
                         <Link className="nav-link" to="/searchresult">Search Result</Link>
                     </li>
                     <li className={window.location.pathname === "/market" ? "active" : ""}>
-                        <Link className="nav-link" to="/market">Market</Link>
+                        <Link className="nav-link" to="/market">My Market</Link>
                     </li>
-                    <li className={window.location.pathname === "/map" ? "active" : ""}>
+                    {/* <li className={window.location.pathname === "/map" ? "active" : ""}>
                         <Link className="nav-link" to="/map">Map</Link>
-                    </li>
+                    </li> */}
                 </ul>
             </div>
         </nav>
