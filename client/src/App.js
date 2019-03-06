c4204b8dad9bce3f05bf711c4b15b9997ce13832
shilpipawar <shilpipawar@gmail.com>
2019-03-06 01:14:23 -0600

    routes setup private route setup redux debug setup market dummy code

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 import "./App.css";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -14,6 +17,26 @@ import Navbar from "./components/Navbar";
 import mHome from "./components/Map/mHome";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+// Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 
 class App extends Component {
   render() {
@@ -31,6 +54,9 @@ class App extends Component {
             <Route exact path="/searchresult" component={SearchResult} />
             <Route exact path="/newmarket" component={NewMarket} />
             {/* The following line won't exist in final version. Just for testing purposes using a generic market. */}
+            {/* <Switch>
+              <PrivateRoute exact path="/market" component={Market} />
+            </Switch> */}
             <Route exact path="/market" component={Market} />
             {/* The following line will take user to a specific market by id */}
             <Route exact path="/markets/:id" component={Market} />
--- client/src/pages/Market.js
+++ client/src/pages/Market.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
 import API from "../utils/API";
 import MarketContainer from "../components/MarketContainer";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../actions/authActions";
 
 class Market extends Component {
+
+    onLogoutClick = e => {
+        e.preventDefault();
+        this.props.logoutUser();
+    };
 
     state = {
         markets: {}
@@ -48,33 +56,56 @@ class Market extends Component {
                     img: "",
                     roles: []
                 }))
-                .catch(err => console.log(err));
+            .catch(err => console.log(err));
     };
 
     render() {
+        const { user } = this.props.auth;
 
         return (
+            //Shilpa Temp changes
             <div className="container-fluid">
-                    <div className="row" key={this.state.markets._id}>
-                        <div className="col-md-12 mx-auto">
-                            <MarketContainer
-                                marketName={this.state.markets.marketName}
-                                organizer={this.state.markets.organizer}
-                                address={this.state.markets.address}
-                                city={this.state.markets.city}
-                                state={this.state.markets.state}
-                                zip={this.state.markets.zip}
-                                about={this.state.markets.about}
-                                img={this.state.markets.img}
-                                roles={this.state.markets.roles}
-                            />
-                            
+                <div className="row" key={this.state.markets._id}>
+                    <div className="col-md-12 mx-auto">
+                        <MarketContainer
+                            marketName={this.state.markets.marketName}
+                            organizer={this.state.markets.organizer}
+                            address={this.state.markets.address}
+                            city={this.state.markets.city}
+                            state={this.state.markets.state}
+                            zip={this.state.markets.zip}
+                            about={this.state.markets.about}
+                            img={this.state.markets.img}
+                            roles={this.state.markets.roles}
+                        />
+
+                    </div>
+                </div>
+                {/* //Shilpa
+                <div style={{ height: "75vh" }} className="container valign-wrapper">
+                    <div className="row">
+                        <div className="col s12 center-align">
+                            <h4>
+                                <b>Hey there,</b> {user.name.split(" ")[0]}
+                            </h4>
                         </div>
                     </div>
+                </div> */}
             </div>
 
         )
     }
 }
 
-export default Market;
+// export default Market;
+Market.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+  };
+  const mapStateToProps = state => ({
+    auth: state.auth
+  });
+  export default connect(
+    mapStateToProps,
+    { logoutUser }
+  )(Market);
