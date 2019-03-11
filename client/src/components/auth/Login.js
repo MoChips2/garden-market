55863fb78bf582c357a86b7ac6de1a95ef494d3f
Brian Nickila <brian.nickila@gmail.com>
2019-03-11 00:15:07 -0500

    Stylized Login page.

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -49,15 +49,15 @@ class Login extends Component {
         <div className="row">
           <div className="col-md-8 mx-auto">
             <div className="card loginCard">
-              <div className="card-header">
+              <div className="card-header loginCardHeader">
                 <h2 className="loginHeader">LOGIN</h2>
               </div>
               <div className="card-body loginCardBody">
                 <h5 className="card-title loginCardTitle">
-                  Don't have an account? <Link to="/register" className="registerLink">REGISTER</Link>
+                  Don't have an account? &nbsp; <Link to="/register" className="registerLink">REGISTER</Link>
                 </h5>
                 <form noValidate onSubmit={this.onSubmit}>
-                  <div>
+                  <div className="emailDiv">
                     <input
                       onChange={this.onChange}
                       value={this.state.email}
@@ -68,13 +68,13 @@ class Login extends Component {
                         invalid: errors.email || errors.emailnotfound
                       })}
                     />
-                    <label htmlFor="email">Email</label>
+                    <label htmlFor="email" className="label">Email</label>
                     <span className="red-text">
                       {errors.email}
                       {errors.emailnotfound}
                     </span>
                   </div>
-                  <div>
+                  <div className="passwordDiv">
                     <input
                       onChange={this.onChange}
                       value={this.state.password}
@@ -85,78 +85,18 @@ class Login extends Component {
                         invalid: errors.password || errors.passwordincorrect
                       })}
                     />
-                    <label htmlFor="password">Password</label>
+                    <label htmlFor="password" className="label">Password</label>
                     <span className="red-text">
                       {errors.password}
                       {errors.passwordincorrect}
                     </span>
                   </div>
                   <div className="loginBtnDiv">
-                    <button type="submit" className="btn btn-primary loginBtn">Login</button>
+                    <button type="submit" className="btn btn-primary loginBtn">LOGIN</button>
                   </div>
                 </form>
               </div>
             </div>
-
-
-
-
-            {/* <div className="col-md-8">
-              <h4>
-                <b>Login</b> below
-              </h4>
-
-            </div>
-            <form noValidate onSubmit={this.onSubmit}>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.email}
-                  error={errors.email}
-                  id="email"
-                  type="email"
-                  className={classnames("", {
-                    invalid: errors.email || errors.emailnotfound
-                  })}
-                />
-                <label htmlFor="email">Email</label>
-                <span className="red-text">
-                  {errors.email}
-                  {errors.emailnotfound}
-                </span>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password}
-                  error={errors.password}
-                  id="password"
-                  type="password"
-                  className={classnames("", {
-                    invalid: errors.password || errors.passwordincorrect
-                  })}
-                />
-                <label htmlFor="password">Password</label>
-                <span className="red-text">
-                  {errors.password}
-                  {errors.passwordincorrect}
-                </span>
-              </div>
-              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                <button
-                  style={{
-                    width: "150px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px",
-                    marginTop: "1rem"
-                  }}
-                  type="submit"
-                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                >
-                  Login
-                </button>
-              </div>
-            </form> */}
           </div>
         </div>
       </div>
