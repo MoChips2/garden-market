4bcd7d3ed4642412b7a8c2c419b8067b808c80d1
MoChips2 <31714669+smedhan1@users.noreply.github.com>
2019-03-11 21:18:03 -0500

    geocoding progress

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
-import googleAPI from "./key";
 
 
 export class MapContainer extends Component {
