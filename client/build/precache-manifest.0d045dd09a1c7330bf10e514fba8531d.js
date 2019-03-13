3e0ca172ee48ae2f656f99ca019e5f800b1a28b6
shilpipawar <shilpipawar@gmail.com>
2019-03-13 01:51:45 -0500

    Save Coord working

---
+++ client/build/precache-manifest.0d045dd09a1c7330bf10e514fba8531d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8513b05b73885fc2f54a",
+    "url": "/static/js/main.8513b05b.chunk.js"
+  },
+  {
+    "revision": "3e7009d61faabf11b083",
+    "url": "/static/js/2.3e7009d6.chunk.js"
+  },
+  {
+    "revision": "8513b05b73885fc2f54a",
+    "url": "/static/css/main.2a40aaf9.chunk.css"
+  },
+  {
+    "revision": "c9248109e665f5cf6652b85a95d4a5bb",
+    "url": "/index.html"
+  }
+];
