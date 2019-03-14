cf5c37894e213a854dad22e63603e63947d30a72
shilpipawar <shilpipawar@gmail.com>
2019-03-13 21:00:12 -0500

    Location worked search work

---
+++ client/build/precache-manifest.48dba7061afc3c5c7adf0b1c032a1765.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3709b1077521dde4a7fb",
+    "url": "/static/js/main.3709b107.chunk.js"
+  },
+  {
+    "revision": "2040d202631d1413669e",
+    "url": "/static/js/2.2040d202.chunk.js"
+  },
+  {
+    "revision": "3709b1077521dde4a7fb",
+    "url": "/static/css/main.9aa21062.chunk.css"
+  },
+  {
+    "revision": "6e0b1940ff2e4a0bf11049d786e95faa",
+    "url": "/index.html"
+  }
+];
