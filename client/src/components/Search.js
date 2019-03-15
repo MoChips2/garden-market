4356d0c4e559a2fd7b0108edb13804668d04f479
shilpipawar <shilpipawar@gmail.com>
2019-03-14 19:01:02 -0500

    Build

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../components/Search.css";
 import { withRouter } from "react-router-dom";
-import SearchResult from "../pages/SearchResult";
+import API from "../utils/API";
 
 // function Search() {
 class Search extends React.Component {
@@ -18,27 +18,28 @@ class Search extends React.Component {
     handleFormSubmit = event => {
         event.preventDefault();
         console.log("Search address :" + this.state.address);
-        if(this.state.address != null){
-            this.props.history.push("/searchresult");
-            // window.open(
-            //     "searchresult",
-            //   )
+        if (this.state.address != null) {
+            let promiseGeoLocation;
+            API.geocodeAddress(this.state.address).then(
+                this.props.history.push("/searchresult")
+            );
+            console.log("promiseGeoLocation--Exit");
         }
     }
-    render() {
-        return (
-            <div className="col-md-12 mx-auto searchDiv">
-                <label className="searchMessage">FIND A MARKET NEAR YOU</label>
-                <div class="input-group">
-                    <input type="text" class="form-control" name="address" placeholder="Roseville MN" value={this.state.address} onChange={this.handleInputChange} />
-                    <span class="input-group-btn">
-                        <button class="btn btn-danger addressSearchBtn" type="button" onClick={this.handleFormSubmit}>
-                        SEARCH</button>
-                    </span>
-                </div>
-            </div >
-        )
+        render() {
+            return (
+                <div className="col-md-12 mx-auto searchDiv">
+                    <label className="searchMessage">FIND A MARKET NEAR YOU</label>
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="address" placeholder="Roseville MN" value={this.state.address} onChange={this.handleInputChange} />
+                        <span class="input-group-btn">
+                            <button class="btn btn-danger addressSearchBtn" type="button" onClick={this.handleFormSubmit}>
+                                SEARCH</button>
+                        </span>
+                    </div>
+                </div >
+            )
+        }
     }
-}
 
-export default withRouter(Search);
+    export default withRouter(Search);
